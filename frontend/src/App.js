d8df2816ca4d5ae71f69b8f0e71bd47b9dac0303
Deyveson Willian <deyvesonn@outlook.com>
2020-03-23 12:42:13 -0400

    end of the first day

---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello OminiStack
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Project OminiStack 11.0 - 23/03
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
