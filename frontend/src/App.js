5ae32d9ce3b7741c96dc4660a84c0961a1b70de7
Deyveson <deyvesonn@outlook.com>
2020-03-29 15:23:49 -0300

    Estado e Imutabilidade react

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './Header';
 
 function App() {
+   const [counter, setCounter] = useState(0);
+
+   // Estado e Imutabilidade 
+   // useState retorna = [valor, function]
+
+   function increment (){
+     setCounter(counter + 1);
+   }
+
    return (
-      <Header title ="RocketSeat">
-         Semana OmniStack
-      </Header>
+      <>
+         <Header title ="RocketSeat">
+            Contador : { counter }
+         </Header>
+         <button onClick= {increment} > Incrementar </button>
+      </>
    );
 }
 
